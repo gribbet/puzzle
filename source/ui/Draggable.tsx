c6a8332ed30deb812443364e5e99765e7bfec01f
Graham Gibbons <graham@figure.works>
2019-02-12 10:09:07 -0800

    Fix Firefox drag behavior

--- source/ui/Draggable.tsx
+++ source/ui/Draggable.tsx
@@ -1,83 +1,85 @@
 import * as React from "react";
 import { Component } from "react";
 import {
-  add,
-  dot,
-  length,
-  normalizeAngle,
-  perpendicular,
-  rotate,
-  subtract,
-  toDegrees
+    add,
+    dot,
+    length,
+    normalizeAngle,
+    perpendicular,
+    rotate,
+    subtract,
+    toDegrees
 } from "../math";
 import { Point } from "../model";
 import { screenToLocal } from "../svg";
 
 export interface IDraggableProps {
-  position: Point;
-  rotation: number;
-  center: Point;
-  radius: number;
-  onMove: (_: { position: Point; rotation: number }) => void;
+    position: Point;
+    rotation: number;
+    center: Point;
+    radius: number;
+    onMove: (_: { position: Point; rotation: number }) => void;
 }
 
 export class Draggable extends Component<IDraggableProps> {
-  private gRef?: SVGGElement;
-  private dragging?: Point;
+    private gRef?: SVGGElement;
+    private dragging?: Point;
 
-  public componentWillMount() {
-    window.addEventListener("mousemove", this.onMouseMove);
-  }
-
-  public componentWillUnmount() {
-    window.removeEventListener("mousemove", this.onMouseMove);
-  }
-
-  public render() {
-    const { position, rotation, children } = this.props;
-    const [x, y] = position;
-
-    return (
-      <g
-        ref={_ => (this.gRef = _ || undefined)}
-        transform={`rotate(${rotation}) translate(${x} ${y})`}
-        onMouseDown={this.onMouseDown}
-      >
-        {children}
-      </g>
-    );
-  }
-
-  private onMouseDown = ({ clientX, clientY }: React.MouseEvent) => {
-    this.dragging = screenToLocal(this.gRef!, [clientX, clientY]);
-  };
-
-  private onMouseMove = (event: MouseEvent) => {
-    if (!event.buttons) {
-      this.dragging = undefined;
+    public componentWillMount() {
+        window.addEventListener("mousemove", this.onMouseMove);
     }
 
-    if (this.dragging === undefined || !this.gRef) {
-      return;
+    public componentWillUnmount() {
+        window.removeEventListener("mousemove", this.onMouseMove);
     }
 
-    const { position, rotation, center, radius, onMove } = this.props;
-    const { clientX, clientY, movementX, movementY } = event;
+    public render() {
+        const { position, rotation, children } = this.props;
+        const [x, y] = position;
 
-    const client: Point = [clientX, clientY];
-    const movement: Point = [movementX, movementY];
-    const dragging = subtract(this.dragging, center);
+        return (
+            <g
+                ref={_ => (this.gRef = _ || undefined)}
+                transform={`rotate(${rotation}) translate(${x} ${y})`}
+                onMouseDown={this.onMouseDown}
+            >
+                {children}
+            </g>
+        );
+    }
 
-    const x0 = screenToLocal(this.gRef, subtract(client, movement));
-    const x = screenToLocal(this.gRef, client);
-    const dr =
-      toDegrees(
-        dot(subtract(x, x0), perpendicular(dragging)) / radius / radius / radius
-      ) * length(dragging);
+    private onMouseDown = (event: React.MouseEvent) => {
+        event.preventDefault();
+        const { clientX, clientY } = event;
+        this.dragging = screenToLocal(this.gRef!, [clientX, clientY]);
+    };
 
-    onMove({
-      position: subtract(rotate(add(x, position), -dr), add(center, dragging)),
-      rotation: normalizeAngle(rotation + dr)
-    });
-  };
+    private onMouseMove = (event: MouseEvent) => {
+        if (!event.buttons) {
+            this.dragging = undefined;
+        }
+
+        if (this.dragging === undefined || !this.gRef) {
+            return;
+        }
+
+        const { position, rotation, center, radius, onMove } = this.props;
+        const { clientX, clientY, movementX, movementY } = event;
+
+        const client: Point = [clientX, clientY];
+        const movement: Point = [movementX, movementY];
+        const dragging = subtract(this.dragging, center);
+
+        const x0 = screenToLocal(this.gRef, subtract(client, movement));
+        const x = screenToLocal(this.gRef, client);
+        const dr =
+            toDegrees(
+                dot(subtract(x, x0), perpendicular(dragging)) / radius / radius / radius
+            ) * length(dragging);
+
+        onMove({
+            position: subtract(rotate(add(x, position), -dr), add(center, dragging)),
+            rotation: normalizeAngle(rotation + dr)
+        });
+    };
 }
